4d48d618010789b2e2963327d49aa6ccc5527a24
vadivel91982 <vadivel82@gmail.com>
2020-05-30 22:17:46 +0530

    react-redux

---
+++ src/action/postActions.js
@@ -0,0 +1,26 @@
+import { FETCH_POSTS, NEW_POST } from './types';
+
+export const fetchPost = () => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts => dispatch({
+            type: FETCH_POSTS,
+            payload: posts
+        }));
+}
+
+export const createPost = (postData) => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+        .then(res => res.json())
+        .then(post => dispatch({
+            type: NEW_POST,
+            payload: post
+        })
+        )
+}
